77d84c3082e4c670c1dda86067e7e891167054b0
Cainã <cainarv@gmail.com>
2026-02-27 15:45:04 -0300

    Menor Numero

---
+++ aula2402.c
@@ -0,0 +1,36 @@
+/*
+    AULA 24/02
+Atividade para re-lembrar aulas passadas
+    Beecrowd 1180
+*/
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Digite o tamanho do vetor: \n");
+    scanf("%d", &n);
+    int x[n];
+
+    printf("Digitar os numeros do vetor: \n");
+    for (int i=0; i<n; i++){
+        scanf("%d", &x[i]);
+        }
+    int menor = x[0];
+    int posicao = 0;
+
+    for (int i=0; i<n; i++){
+        if (menor > x[i]){
+            menor = x[i];
+            posicao = i;
+        }
+        
+    }
+    
+
+    printf("\nSeus Resultados sao: \n");
+    printf("Menor valor: %i\n", menor);
+    printf("Posicao: %i\n", posicao);
+    
+
+
+}
